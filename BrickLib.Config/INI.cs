17850832f790fb1aa47328dc4778855824c1c5bf
José Osorio <darkguy2008@gmail.com>
2014-01-16 20:31:15 -0430

    Initial upload
    First upload with a first class, used to handle and read INI files in a
    dictionary-like intuitive way.

---
+++ BrickLib.Config/INI.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BrickLib.Config
+{
+    public class INI
+    {
+        private Dictionary<String, List<KeyValuePair<String, String>>> INIFile = new Dictionary<String, List<KeyValuePair<String, String>>>();
+
+        public INI() { }
+        public INI(String filename)
+        {
+            Load(filename);
+        }
+
+        public void Load(String filename)
+        {
+            foreach (String line in File.ReadAllLines(filename))
+            {
+                String l = line.Trim();
+                if (l.StartsWith("#"))
+                    continue;
+                else if (l.StartsWith("["))
+                {
+                    INIFile[l.Substring(1, l.Length - 2)] = new List<KeyValuePair<String, String>>();
+                }
+                else if (l.Contains("="))
+                {
+                    INIFile[INIFile.Keys.Last()].Add(new KeyValuePair<String, String>(
+                        l.Substring(0, l.IndexOf('=')),
+                        l.Substring(l.IndexOf('=') + 1)
+                        )
+                    );
+                }
+            }
+        }
+
+        public class ValueItem
+        {
+            public String Value;
+            public String[] Array;
+        }
+
+        public ValueItemCollection this[String key]
+        {
+            get
+            {
+                return new ValueItemCollection(INIFile.Where(x => x.Key.ToLowerInvariant().Trim() == key.ToLowerInvariant().Trim()).Single().Value);
+            }
+        }
+
+        public class ValueItemCollection
+        {
+            private List<KeyValuePair<String, String>> _items = new List<KeyValuePair<String, String>>();
+
+            public ValueItemCollection(List<KeyValuePair<String, String>> items)
+            {
+                _items = items;
+            }
+
+            public ValueItem this[String key]
+            {
+                get {
+                    ValueItem rv = new ValueItem();
+                    List<String> li = _items.Where(x => x.Key.ToLowerInvariant().Trim() == key.ToLowerInvariant().Trim()).Select(x => x.Value).ToList();
+                    if (li.Count > 1)
+                        rv.Array = li.ToArray();
+                    else
+                        rv.Value = li.Single();
+                    return rv;
+                }
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using BrickLib.Config;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INI i = new INI("test.ini");
+            var obj1 = i["test"]["Key"].Value;
+            var obj2 = i["Test"]["Key2"].Value;
+            var obj3 = i["Test"]["Multival"].Array;
+        }
+    }
+}
