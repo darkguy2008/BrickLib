122c3d2ef06445116868db7826d10f1fd87fe6ff
Alemar <darkguy2008@gmail.com>
2014-06-30 16:06:10 -0430

    * Added new BrickLib.Web library with some helper methods taken from my homepage

---
+++ BrickLib.Web/TagHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+
+namespace BrickLib.Web
+{
+    public static class TagHelpers
+    {
+        public static void AddCSS(this Control cph, String filename)
+        {
+            HtmlLink lnk = new HtmlLink();
+            lnk.Href = cph.ResolveUrl(filename);
+            lnk.Attributes.Add("rel", "stylesheet");
+            lnk.Attributes.Add("type", "text/css");
+            cph.Controls.Add(lnk);
+        }
+        public static void AddJS(this Control cph, String filename)
+        {
+            HtmlGenericControl ctl = new HtmlGenericControl();
+            ctl.TagName = "script";
+            ctl.Attributes["type"] = "text/javascript";
+            ctl.Attributes["src"] = cph.ResolveUrl(filename);
+            cph.Controls.Add(ctl);
+        }
+    }
+
+    /*
+    public static partial class Extensions
+    {
+        public static string RelativePath(this HttpServerUtility srv, string path, HttpRequest context)
+        {
+            return path.Replace(context.ServerVariables["APPL_PHYSICAL_PATH"], "~/").Replace(@"\", "/");
+        }
+    }
+    */
+}
