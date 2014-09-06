d7a1eb93aec6f366b1dfa72cde24fff128684549
DARKGuy <darkguy2008@gmail.com>
2014-09-06 10:11:11 -0430

    Refactor + new stuff

---
+++ BrickLib.Web/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrickLib.Web
+{
+    public static class Extensions
+    {
+        // public static string RelativePath(this HttpServerUtility srv, string path, HttpRequest context) { return path.Replace(context.ServerVariables["APPL_PHYSICAL_PATH"], "~/").Replace(@"\", "/"); }
+
+        // TODO: Optimize with BPs (best-practices)
+        public static String ToQueryString(this Dictionary<String, String> dict)
+        {
+            String rv = String.Empty;
+
+            if (dict.Count > 0)
+            {
+                rv = "?";
+                foreach (KeyValuePair<String, String> kv in dict)
+                    rv += kv.Key + "=" + kv.Value + "&";
+                rv = rv.Substring(0, rv.Length - 1);
+            }
+
+            return rv;
+        }
+    }
+}
--- BrickLib.Web/TagHelpers.cs
+++ BrickLib.Web/TagHelpers.cs
@@ -7,15 +7,38 @@ using System.Web.UI.HtmlControls;
 
 namespace BrickLib.Web
 {
+    public enum Conditionals
+    {
+        None = 0,
+        IE6 = 1,
+        IE7,
+        IE8,
+        IE9,
+        IE10
+    }
+
     public static class TagHelpers
     {
-        public static void AddCSS(this Control cph, String filename)
+        public static void AddCSS(this Control cph, String filename, Conditionals cond = Conditionals.None, bool absolute = false)
         {
-            HtmlLink lnk = new HtmlLink();
-            lnk.Href = cph.ResolveUrl(filename);
-            lnk.Attributes.Add("rel", "stylesheet");
-            lnk.Attributes.Add("type", "text/css");
-            cph.Controls.Add(lnk);
+            if (cond == Conditionals.None)
+            {
+                HtmlLink lnk = new HtmlLink();
+                lnk.Href = absolute ? filename : cph.ResolveUrl(filename);
+                lnk.Attributes.Add("rel", "stylesheet");
+                lnk.Attributes.Add("type", "text/css");
+                cph.Controls.Add(lnk);
+            }
+            else
+            {
+                String ifComment = String.Empty;
+                if (cond == Conditionals.IE6) { ifComment = "<!--[if IE 6]>"; }
+                if (cond == Conditionals.IE7) { ifComment = "<!--[if IE 7]>"; }
+                if (cond == Conditionals.IE8) { ifComment = "<!--[if IE 8]>"; }
+                if (cond == Conditionals.IE9) { ifComment = "<!--[if IE 9]>"; }
+                if (cond == Conditionals.IE10) { ifComment = "<!--[if IE 10]>"; }
+                cph.Controls.Add(new LiteralControl(ifComment + "<link rel=\"stylesheet\" href=\"" + (absolute ? filename : cph.ResolveUrl(filename)) + "\"><![endif]-->"));
+            }
         }
         public static void AddJS(this Control cph, String filename)
         {
@@ -26,14 +49,4 @@ namespace BrickLib.Web
             cph.Controls.Add(ctl);
         }
     }
-
-    /*
-    public static partial class Extensions
-    {
-        public static string RelativePath(this HttpServerUtility srv, string path, HttpRequest context)
-        {
-            return path.Replace(context.ServerVariables["APPL_PHYSICAL_PATH"], "~/").Replace(@"\", "/");
-        }
-    }
-    */
 }
